0fd7f3ca9e7a84d2e2a5c1a8208ebe91df210053
znibb <pontus.pson@gmail.com>
2018-04-25 22:42:56 +0200

    Changed install script to use whiptail menu

--- install.sh
+++ install.sh
@@ -11,8 +11,31 @@ script_path=$(dirname $(realpath $0))
 apt update
 touch /tmp/apt_updated
 
-$script_path/./setup_i3.sh $home_path
-$script_path/./setup_vim.sh $home_path
+selected_list=($(whiptail --title "Dotfiles installer" --separate-output --checklist \
+"Choose modules to install" 20 78 4 \
+"i3" "i3wm with i3blocks etc" OFF \
+"vim" "vim config" OFF \
+"alias" "Common aliases for bash" OFF \
+"alias_rpi" "RPi-specific aliases" OFF \
+3>&1 1>&2 2>&3))
+
+for i in "${selected_list[@]}"
+do
+	case "$i" in
+		"i3")
+			$script_path/./setup_i3.sh $home_path
+			;;
+		"vim")
+			$script_path/./setup_vim.sh $home_path
+			;;
+		*)
+			echo "invalid: $i"
+			;;
+	esac
+done
+
+
+
 
 rm /tmp/apt_updated
 exit 0
--- setup_i3.sh
+++ setup_i3.sh
@@ -14,19 +14,20 @@ if [ ! -f /tmp/apt_updated ]; then
 fi
 
 # Install packages
-apt install -y 	i3 \ 			# Duh
-		vim \ 			# Also duh
-		pulseaudio-utils \ 	# For media controls
-		feh \ 			# Displaying wallpapers
-		arandr \ 		# Helps with multi-monitor setup
-		lxappearance \ 		# Handle themes
-		arc-theme \ 		# Nice gtk theme
-		moka-icon-theme		# Icon theme pack
-		rofi \			# Dmenu replacement
-		comption \		# Allows for opaque windows
-		i3blocks \		# Better option for status bar than i3status
-		scrot \			# Screen capture tool for generating blurred lock screen
-		imagemagick		# For blurring lock screen
+apt install -y \
+	i3 \
+	vim \
+	pulseaudio-utils \
+	feh \
+	arandr \
+	lxappearance \
+	arc-theme \
+	moka-icon-theme \
+	rofi \
+	compton \
+	i3blocks \
+	scrot \
+	imagemagick
 
 # Install playerctl
 dpkg -i "$script_path/tools/playerctl-0.5.0_amd64.deb"	# For media control
@@ -36,7 +37,7 @@ mkdir -p $home_path/.fonts
 cp $script_path/fonts/* $home_path/.fonts/
 mkdir -p $home_path/.config/i3
 cp $script_path/i3/i3.conf $home_path/.config/i3/config
-cp $script_path/i3/i3blocks.conf $home_path/.config/i3.i3blocks.conf
+cp $script_path/i3/i3blocks.conf $home_path/.config/i3/i3blocks.conf
 mkdir -p $home_path/scripts/i3blocks
 cp $script_path/i3/i3blocks_scripts/* $home_path/scripts/i3blocks/
 cp $script_path/scripts/i3lock.sh $home_path/scripts/i3lock.sh
