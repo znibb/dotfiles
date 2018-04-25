4a7b14c3ebdba5a953b4cfded3b89e9b686df8ee
znibb <pontus.pson@gmail.com>
2018-04-25 10:42:36 +0200

    Fixed missing forward of home path argument to sub scripts

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ script_path=$(dirname $(realpath $0))
 apt update
 touch /tmp/apt_updated
 
-$script_path/./setup_i3.sh
-$script_path/./setup_vim.sh
+$script_path/./setup_i3.sh $home_path
+$script_path/./setup_vim.sh $home_path
 
 rm /tmp/apt_updated
 exit 0
