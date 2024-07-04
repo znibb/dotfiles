a8fc5a4cf8563e62b3d75670a4bc711e742f2c77
Pontus Petersson <pontus.pson@gmail.com>
2024-07-04 17:20:59 +0200

    Fixed setup scripts for tmux and vim

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-	echo "Error: no home path"
-	exit 1
-fi
-
-home_path=$1
 script_path=$(dirname $(realpath $0))
 
-# If batching "apt update" has already been run
-if [ ! -f /tmp/apt_updated ]; then
-	apt update
-fi
-apt install -y vim
+dpkg -l vim > /dev/null 2>&1 || sudo apt install vim
 
-cp $script_path/vim/.vimrc $home_path/
+cd $HOME
+
+ln -s -f $script_path/vim/.vimrc .vimrc
