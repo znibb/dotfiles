8571eb07ed4e7ae8241ceb139fa6e61ee5fc94d2
Pontus Berlin <pontus@b3rl1n.com>
2024-10-04 13:18:00 +0200

    WIP

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 script_path=$(dirname $(realpath $0))
-target_path=~/.config/zsh
+target_path=~/.config
 
 # Install package if not already available
 which zsh > /dev/null 2>&1 || sudo apt install -y zsh
@@ -11,13 +11,14 @@ grep "export ZDOTDIR" ~/.profile > /dev/null|| echo '
 # Set zsh config dir
 export ZDOTDIR=$HOME/.config/zsh' | tee -a ~/.profile > /dev/null
 
-# Install oh-my-zsh
-[ -d $target_path/ohmyzsh ] || sh -c "ZSH=$HOME/.config/zsh/ohmyzsh $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && sleep 5s
+# Install starship if not already available
+which /usr/local/bin/starship > /dev/null 2>&1 || curl -sS https://starship.rs/install.sh | sh
 
 # Link config files
-mkdir -p $target_path
-ln -s -f $script_path/zsh/.zshrc $target_path/.zshrc
-ln -s -f $script_path/zsh/.aliases $target_path/.aliases
+mkdir -p $target_path/zsh
+ln -s -f $script_path/zsh/.zshrc $target_path/zsh/.zshrc
+ln -s -f $script_path/zsh/.aliases $target_path/zsh/.aliases
+ln -s -f $script_path/zsh/starship.toml $target_path/starship.toml
 
 # Change default shell
 sudo chsh -s $(which zsh) $USER
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -7,30 +7,8 @@ ZSH_THEME="robbyrussell"
 HISTFILE="$ZDOTDIR/.zsh_history"
 HIST_STAMPS="yyyy-mm-dd"
 
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
-
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
-
-source $ZSH/oh-my-zsh.sh
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='nvim'
-# fi
-
-# Remove aliases included with ohmyzsh
-unalias -m '*'
+# Activate Starship
+eval "$(starship init zsh)"
 
 # Source aliases
 [ -f $ZDOTDIR/.aliases ] && source $ZDOTDIR/.aliases
