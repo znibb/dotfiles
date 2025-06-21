4fe1eb2d3c46bfb96fc9813de4a59aa9d70b41c0
Pontus Berlin <pontus@b3rl1n.com>
2025-06-21 17:41:08 +0200

    Added green gruvbox theme

--- roles/zsh/files/themes/createThemes.sh
+++ roles/zsh/files/themes/createThemes.sh
@@ -2,3 +2,4 @@
 
 /bin/cat gruvbox.toml palette-rainbow.toml > gruvbox-rainbow.toml
 /bin/cat gruvbox.toml palette-blue.toml > gruvbox-blue.toml
+/bin/cat gruvbox.toml palette-green.toml > gruvbox-green.toml
