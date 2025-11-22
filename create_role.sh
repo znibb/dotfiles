a8adbaa82617bc8b43f4b0712dfb750de23f0744
Pontus Berlin <pontus@b3rl1n.com>
2025-11-22 14:06:44 +0100

    Updated create_role script to use new ansible_facts format

--- create_role.sh
+++ create_role.sh
@@ -3,4 +3,4 @@
 mkdir -p ./roles/$1/{files,tasks,vars}
 touch ./roles/$1/tasks/main.yml
 touch ./roles/$1/vars/main.yml
-echo "$1_config_dir: \"{{ ansible_env.HOME }}/.config/$1\"" > ./roles/$1/vars/main.yml
+echo "$1_config_dir: \"{{ ansible_facts.env.HOME }}/.config/$1\"" > ./roles/$1/vars/main.yml
